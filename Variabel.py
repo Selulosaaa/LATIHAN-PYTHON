d7ed7d18b6c5b5697975e7b9c7424bc76ac9b74e
SeluLosa <dananputra213@gmail.com>
2026-09-17 22:31:53 +0700

    update Variabel.py

---
+++ Variabel.py
@@ -0,0 +1,20 @@
+#HALO kali ini kita akan belajar variabel dan juga dapat bersama print
+#apa itu variabel? variabel intinya adalah tempat untuk menyimpan sebuah data
+#dengan format (nama_variabel = "isi") sama seperti print, jika berubah str(angka) maka tak perlu paka tanda petik
+
+#contoh variabel
+nama = "Rahmat"
+umur = 16 #string atau angka bulat
+tempat_tinggal = "yogyakarta" #untuk spasi, kita menggunakan underscore
+#lalu untuk memanggil nama variabelnya dapat menggunakan variasi dengan print
+
+print(nama)
+print(umur)
+print(tempat_tinggal)
+#output nya akan memunculkan ketiga isi variabel tersebut
+print("-" * 20)
+#variable dapat digabungkan dengan satu baris langsung dengan print
+print("nama kamu adalah", nama)
+print(f"nama kamu adalah {nama}") #f untuk mempermudah saja agar bisa langsung
+
+
