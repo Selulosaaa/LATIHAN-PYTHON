1a506bff597f4f6068740bd1fad0bca22a72c764
SeluLosa <dananputra213@gmail.com>
2026-09-17 22:10:07 +0700

    update Print.py

--- Print.py
+++ Print.py
@@ -1,1 +1,17 @@
+#selamat datang di python, disini kira akan mempelajari print
+
+#print berguna untuk mencetak atau memerintahkan memunculkan sebuah teks
+#hal tersebut dapat diwujudkan dengan format print("teks")
+#dapat dipraktekkan
+
 print("hello world")
+#output "hello world"
+
+#lalu untuk memunculkan sebuah angka tidak perlu pakai tanda petik!
+
+print(100)
+#output 100
+
+#jika ingin memunculkan teks lebi dari 1 dalam satu baris maka bisa dipakai (*)
+
+print("-" * 20) #menandakan di print 20 kali
