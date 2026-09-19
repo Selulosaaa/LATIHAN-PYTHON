6033e0855a17030563399e9b6963cd12a28ea5c6
SeluLosa <dananputra213@gmail.com>
2026-09-19 23:03:38 +0700

    update proyek/sum.py

---
+++ proyek/sum.py
@@ -0,0 +1,4 @@
+first_number = input("beri nomor pertama ")
+second_number = input("beri nomor ke dua ")
+hasil = float(first_number) + float(second_number)
+print("hasil opeasi tambah adalah", hasil)
