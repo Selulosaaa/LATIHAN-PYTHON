49afe7be76b79a194c95f258173d88ef3bd4d439
SeluLosa <dananputra213@gmail.com>
2026-09-19 23:03:46 +0700

    update proyek/Variabel.py

--- proyek/Variabel.py
+++ proyek/Variabel.py
@@ -1,1 +1,10 @@
+#Latihan proyek kali ini adalah variabel
+#bayangkan kamu dirumah sakit, kamu harus menyimpan nama pasien, beserta umur dan dia adalah pasien baru.
 
+nama = "John smith"
+umur = 20
+pasien_baru = not True
+
+print(nama)
+print(umur)
+print(pasien_baru)
