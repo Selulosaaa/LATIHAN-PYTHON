f434b4e1579a2edd635966daf6f06e703a306724
SeluLosa <dananputra213@gmail.com>
2026-09-22 20:05:51 +0700

    update aritmatika.py

---
+++ aritmatika.py
@@ -0,0 +1,29 @@
+#Penjumlahan (+): Menjumlahkan dua buah nilai. Contoh: 5 + 3 menghasilkan 8.
+#Pengurangan (-): Mengurangi nilai di kiri dengan nilai di kanan. Contoh: 5 - 3 menghasilkan 2.
+#Pembagian (/): Membagi nilai di kiri dengan nilai di kanan (hasil berupa pecahan/float). Contoh: 5 / 2 menghasilkan 2.5.
+#Pembagian Bulat (//): Membagi angka dan membulatkan hasil ke bawah menjadi bilangan bulat. Contoh: 5 // 2 menghasilkan 2.
+#Sisa Bagi / Modulus (%): Mencari sisa dari hasil pembagian 
+
+x = 10
+y = 20
+print("terdapat x = 10 dan y = 20")
+print("kita coba pada operasi matematika")
+print("-" * 20)
+print("pertambahan")
+tambah = x + y
+print(tambah)
+print("-" * 20)
+print("pengurangan")
+kurang = x - y
+kurang2 = y - x
+print(kurang)
+print(f"hasil bulat y - x = {kurang2}")
+print("-" * 20)
+print("perkalian")
+perkalian = x * y
+print(perkalian)
+print("-" * 20)
+Pembagian = y / x
+print(f"hasil dari y/x adalah= {pembagian}")
+print("-" * 20)
+print("SEPERTI ITULAH ARITMATIKA DALAM PYTHON!!!")
