df15d1d69811e43ed197775969b57451f1a73cd9
SeluLosa <dananputra213@gmail.com>
2026-09-22 21:52:00 +0700

    update conditional.py

---
+++ conditional.py
@@ -0,0 +1,21 @@
+#kondisi adalah percabangan python pakai if-elif-else
+#Pengambilan keputusan (kondisi if) digunakan untuk mengantisipasi kondisi yang terjadi saat jalanya program dan menentukan tindakan 
+#apa yang akan diambil sesuai dengan kondisi.
+x = 18
+if x >= 18:
+  print("kamu sudah dewasa")
+else:
+  print("kamu belum dewasa")
+
+#elif pengambilan lebih dari satu
+temperatur = 20
+
+if temperatur > 35:
+  print("hari yang panas!")
+  print("minum air yang banyak!")
+elif temperatur > 25:
+  print("hari yang cerah!")
+  print("halo dunia! selamat siang!")
+else:
+  print("cuaca yang dingin! segera pakai jaket!")
+  
