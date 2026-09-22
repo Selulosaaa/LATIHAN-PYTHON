055ee8229494b56414856e69d8723666fed73709
SeluLosa <dananputra213@gmail.com>
2026-09-22 20:22:37 +0700

    update perbandingan.py

---
+++ perbandingan.py
@@ -0,0 +1,23 @@
+#Dalam python ada operasi perbandingan, yaitu membandingkan 2 benda/kata/variabel
+#hasilnya hanya 2 yaitu true(benar) dan false(salah)
+
+#operasi perbandingan
+print("perbandingan sama dengan ==")
+x = 3 == 3
+y = 3 == 4
+print(x) #hasil akan true karena 3 sama dengan 3
+print(y) #hasil akan false karena 3 tidak sama dengan dengan 4
+
+#dapat membandingkan dengan kata! pastikan huruf kapital atau huruf kecil harus sama
+z = "halo"
+a = "hi"
+j = "halo"
+print(z == j) #hasil akan true karena kata sama
+print(z == a) #hasil akan false karena kata yang berbeda
+print("_" * 20)
+print("operasi tidak sama dengan !=")
+x = 4
+y = 3
+print(x != y) #hasil true karena memang tidak sama
+print(3 != 3) #hasil false karena mereka sama
+print("_" * 20)
